f5343e7b1cb5b10cd496bd1b016a30f61c876c8a
BrettHutch22 <40153880+hutcb591@users.noreply.github.com>
2025-11-03 09:55:35 -0700

    Update scripts.ts

--- data/mods/gen9aslnatdexdraft/scripts.ts
+++ data/mods/gen9aslnatdexdraft/scripts.ts
@@ -1,4 +1,4 @@
 export const Scripts: BattleScriptsData = {
 	gen: 9,
-	inherit: 'gen9','gen8', 'gen7', 'gen6', 'gen5', 'gen4', 'gen3', 'gen2', 'gen1'
+	inherit: 'gen9','gen8', 'gen7', 'gen6', 'gen5', 'gen4', 'gen3', 'gen2', 'gen1',
 };
