f3819548cb531adaef2cd863aedc0720a585ed1b
hutcb591 <brett.hutchinson22@icloud.com>
2026-03-07 11:18:45 -0700

    Mega Typhlosion and Honey Trap Fix

--- data/abilities.ts
+++ data/abilities.ts
@@ -1868,9 +1868,10 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		num: 118,
 	},
 	honeytrap: {
+		onTryHitPivot: 4,
 		onTryHit(pokemon, target, move) {
 			if (move.flags['pivot']) {
-				this.add('-immune', pokemon, '[from] ability: Honey Trap');
+				this.add('-fail', move, '[from] ability: Honey Trap');
 				return null;
 			}
 		},
