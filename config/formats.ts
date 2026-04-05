cd21757aaae679bf6a3ddcf03102f268e676f927
hutcb591 <brett.hutchinson22@icloud.com>
2026-04-04 22:47:55 -0600

    Relicanth Format Update 4/4/26

--- config/formats.ts
+++ config/formats.ts
@@ -451,6 +451,13 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		ruleset: ['Standard Draft', '+Unobtainable', '+Past', 'Min Source Gen = 1'],
 	},
 	{
+		name: "[Gen 9] ASL Relicanth Draft",
+		mod: 'gen9aslrelicanthdraft',
+		searchShow: false,
+		teraPreviewDefault: true,
+		ruleset: ['Standard Draft', '+Unobtainable', '+Past', 'Min Source Gen = 1'],
+	},
+	{
 		name: "[Gen 9] NatDex 6v6 Doubles Draft",
 		mod: 'gen9',
 		gameType: 'doubles',
