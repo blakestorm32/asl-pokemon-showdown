a6a3177d582f31fd004570030f96046e7409db22
hutcb591 <brett.hutchinson22@icloud.com>
2026-06-28 00:15:43 -0600

    fix

--- config/formats.ts
+++ config/formats.ts
@@ -452,7 +452,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 	},
 	{
 		name: "[Gen 9] v1.1 Champions NatDex Draft",
-		mod: 'gen9-1.1championsnatdexdraft',
+		mod: 'gen9v11championsnatdexdraft',
 		searchShow: false,
 		teraPreviewDefault: true,
 		ruleset: ['Standard Draft', '+Unobtainable', '+Past', 'Min Source Gen = 1'],
